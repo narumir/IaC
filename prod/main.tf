548a8071ba5d4a8e3bba7492dc38b1b12018cd8c
= <=>
2022-11-18 13:40:49 +0000

    add random provider and update aws provider version

--- prod/main.tf
+++ prod/main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 4.40"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
     }
   }
   required_version = ">= 1.2.0"
--- prod/variable.tf
+++ prod/variable.tf
@@ -5,3 +5,5 @@ variable "AWS_ACCESS_KEY_ID" {
 variable "AWS_SECRET_ACCESS_KEY" {
   type = string
 }
+
+resource "random_uuid" "blog_frontend_s3" {}
