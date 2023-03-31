7ac68db11dc7c5b009282de38260e65769e7dd68
narumir <narumir@narumir.io>
2023-03-31 13:44:46 +0000

    apply terraform fmt

--- prod/lambda.tf
+++ prod/lambda.tf
@@ -1,8 +1,8 @@
 resource "aws_lambda_function" "blog_ssr" {
-  s3_key           = "blog.zip"
-  function_name    = "blog-ssr"
-  role             = aws_iam_role.lambda_exec.arn
-  handler          = "server.handler"
+  s3_key        = "blog.zip"
+  function_name = "blog-ssr"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "server.handler"
   # source_code_hash = filebase64sha256("blog.zip")
   source_code_hash = data.archive_file.blog_frontend_code.output_base64sha256
   runtime          = "nodejs16.x"
