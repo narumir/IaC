85f5f62e25ef131decb5f81044c8c6f5b85945ec
narumir <narumir@narumir.io>
2023-07-13 13:33:07 +0000

    may not need declation

--- vpc.tf
+++ vpc.tf
@@ -24,28 +24,3 @@ data "aws_subnet" "seoul_public_subnet_3" {
   cidr_block        = "172.31.32.0/20"
   availability_zone = "ap-northeast-2c"
 }
-
-# data "aws_internet_gateway" "seoul_igw" {
-#   id     = "igw-c41fb9ac"
-#   vpc_id = data.aws_vpc.default_vpc.id
-# }
-
-# data "aws_route_table" "seoul_public_table" {
-#   id     = "rtb-9c2890f7"
-#   vpc_id = data.aws_vpc.default_vpc.id
-# }
-
-# resource "aws_route_table_association" "seoul_public_1" {
-#   subnet_id      = data.aws_subnet.seoul_public_subnet_1.id
-#   route_table_id = aws_route_table.seoul_public_table.id
-# }
-
-# resource "aws_route_table_association" "seoul_public_2" {
-#   subnet_id      = data.aws_subnet.seoul_public_subnet_2.id
-#   route_table_id = aws_route_table.seoul_public_table.id
-# }
-
-# resource "aws_route_table_association" "seoul_public_3" {
-#   subnet_id      = data.aws_subnet.seoul_public_subnet_3.id
-#   route_table_id = aws_route_table.seoul_public_table.id
-# }
