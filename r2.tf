9a751b5380683d12293a1fd56a530e8c4073196a
narumir <narumir@narumir.io>
2023-07-15 08:56:41 +0000

    add blog static bucket record

--- r2.tf
+++ r2.tf
@@ -1,4 +1,5 @@
-resource "aws_s3_bucket" "test_r2" {
-  bucket   = "test-r2-b"
+resource "aws_s3_bucket" "blog_static" {
+  bucket   = "blog-static"
   provider = aws.cloudflare_r2
+  # 아직 cloudflare에서 도메인 연결 부분을 지원하지 않으므로 수동으로 등록 해야함.
 }
