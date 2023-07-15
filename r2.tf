c12656dcfb993cbb21567e121cd3edfba3bc2516
narumir <narumir@narumir.io>
2023-07-15 13:17:16 +0000

    add r2 cors policy

--- r2.tf
+++ r2.tf
@@ -3,3 +3,16 @@ resource "aws_s3_bucket" "blog_static" {
   provider = aws.cloudflare_r2
   # 아직 cloudflare에서 도메인 연결 부분을 지원하지 않으므로 수동으로 등록 해야함.
 }
+
+resource "aws_s3_bucket_cors_configuration" "blog_static_cors" {
+  bucket   = aws_s3_bucket.blog_static.id
+  provider = aws.cloudflare_r2
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = [
+      "https://*.narumir.io",
+      "http://localhost:3000",
+      "https://*.execute-api.ap-northeast-2.amazonaws.com"
+    ]
+  }
+}
