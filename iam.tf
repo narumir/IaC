3e5650f80646f48db9590816f5580e7e753d3fd7
narumir <narumir@narumir.io>
2023-07-22 04:37:00 +0000

    add iam policies for cloudwatch log

--- iam.tf
+++ iam.tf
@@ -9,7 +9,37 @@ data "aws_iam_policy_document" "lambda_exec" {
   }
 }
 
+data "aws_iam_policy_document" "blog_ssr" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:CreateLogGroup",
+      "logs:TagResource"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.blog_ssr.arn
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:PutLogEvent"
+    ]
+    resources = [
+      aws_cloudwatch_log_group.blog_ssr.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "blog_ssr" {
+  name   = "blog_ssr"
+  path   = "/"
+  policy = data.aws_iam_policy_document.blog_ssr.json
+}
+
 resource "aws_iam_role" "lambda_exec" {
-  name               = "labmda_exec"
-  assume_role_policy = data.aws_iam_policy_document.lambda_exec.json
+  name                = "labmda_exec"
+  assume_role_policy  = data.aws_iam_policy_document.lambda_exec.json
+  managed_policy_arns = [aws_iam_policy.blog_ssr.arn]
 }
