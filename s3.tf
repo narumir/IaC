93d964b4a9275cf8ab052b14f2738c33da784d14
narumir <narumir@narumir.io>
2023-07-23 00:58:36 +0000

    add s3 bucket policy

--- s3.tf
+++ s3.tf
@@ -16,23 +16,27 @@ data "aws_s3_object" "blog_ssr_source_code" {
   key    = "blog_ssr.zip"
 }
 
-# data "aws_iam_policy_document" "blog_ssr_bucket" {
-#   statement {
-#     actions = ["s3:*"]
-#     effect  = "Deny"
-#     resources = [
-#       "arn:aws:s3::${aws_s3_bucket.blog_ssr.arn}/*",
-#       "arn:aws:s3:::${aws_s3_bucket.blog_ssr.arn}"
-#     ]
-#     condition {
-#       test     = "Bool"
-#       variable = "aws:SecureTransport"
-#       values   = ["false"]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "blog_ssr_bucket" {
+  statement {
+    actions = ["s3:*"]
+    effect  = "Deny"
+    resources = [
+      "${aws_s3_bucket.blog_ssr.arn}/*",
+      "${aws_s3_bucket.blog_ssr.arn}"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
 
-# resource "aws_s3_bucket_policy" "blog_ssr" {
-#   bucket = aws_s3_bucket.blog_ssr.id
-#   policy = data.aws_iam_policy_document.blog_ssr_bucket.json
-# }
+resource "aws_s3_bucket_policy" "blog_ssr" {
+  bucket = aws_s3_bucket.blog_ssr.id
+  policy = data.aws_iam_policy_document.blog_ssr_bucket.json
+}
