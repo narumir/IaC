3ee99ee4f1bc0f3acd068ec88c6b74b59df896d8
narumir <narumir@narumir.io>
2023-08-07 07:09:26 +0000

    create code store for code deploy

--- s3.tf
+++ s3.tf
@@ -40,3 +40,16 @@ resource "aws_s3_bucket_policy" "blog_ssr" {
   bucket = aws_s3_bucket.blog_ssr.id
   policy = data.aws_iam_policy_document.blog_ssr_bucket.json
 }
+
+resource "aws_s3_bucket" "narumir_io_code_store" {
+  bucket = "narumir-io-code-store"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "narumir_io_code_store" {
+  bucket = aws_s3_bucket.narumir_io_code_store.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
