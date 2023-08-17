2705c0e56dc65d0bb8d74c9e49bd5246eae160de
narumir <narumir@narumir.io>
2023-08-17 18:16:57 +0000

    remove aws lambda for blog ssr

--- api-log-group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "blog_ssr" {
-  name              = "/aws/lambda/${aws_lambda_function.blog_ssr.function_name}"
-  retention_in_days = 30
-}
--- r2.tf
+++ r2.tf
@@ -1,22 +1,3 @@
-resource "aws_s3_bucket" "blog_static" {
-  bucket   = "blog-static"
-  provider = aws.cloudflare_r2
-  # 아직 cloudflare에서 도메인 연결 부분을 지원하지 않으므로 수동으로 등록 해야함.
-}
-
-resource "aws_s3_bucket_cors_configuration" "blog_static_cors" {
-  bucket   = aws_s3_bucket.blog_static.id
-  provider = aws.cloudflare_r2
-  cors_rule {
-    allowed_methods = ["GET"]
-    allowed_origins = [
-      "https://*.narumir.io",
-      "http://localhost:3000",
-      "https://*.execute-api.ap-northeast-2.amazonaws.com"
-    ]
-  }
-}
-
 resource "aws_s3_bucket" "blog_content" {
   bucket   = "blog-content"
   provider = aws.cloudflare_r2
