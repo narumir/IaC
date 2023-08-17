23a7b15b273eed323c019d6f4fe186e442fd5c6e
narumir <narumir@narumir.io>
2023-08-17 19:04:25 +0000

    add frontend deploy

--- code-deploy.tf
+++ code-deploy.tf
@@ -25,3 +25,26 @@ resource "aws_codedeploy_deployment_group" "blog_backend" {
     }
   }
 }
+
+resource "aws_codedeploy_deployment_group" "blog_backend" {
+  app_name               = aws_codedeploy_app.blog_backend.name
+  deployment_group_name  = "deploy_frontend"
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
+  deployment_config_name = "CodeDeployDefault.AllAtOnce"
+  deployment_style {
+    deployment_option = "WITHOUT_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
+  }
+  ec2_tag_set {
+    ec2_tag_filter {
+      type  = "KEY_AND_VALUE"
+      key   = "Type"
+      value = "backend"
+    }
+    ec2_tag_filter {
+      type  = "KEY_AND_VALUE"
+      key   = "Service"
+      value = "blog"
+    }
+  }
+}
